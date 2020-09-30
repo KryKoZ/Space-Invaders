6faa307da3d7dd49faad6ae3d88ad9a3b05a8008
KryKoZ <julian.monginoux@hotmail.com>
2020-09-30 08:12:54 +0200

    first commit

---
+++ BirchEngine - Textures/BirchEngine/Src/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include "SDL_image.h"
+#include <SDL.h>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int width, int height, bool fullscreen);
+
+	void handleEvents();
+	void update();
+	bool running() { return isRunning; }
+	void render();
+	void clean();
+
+private:
+	bool isRunning = false;
+	int cnt = 0;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+};
